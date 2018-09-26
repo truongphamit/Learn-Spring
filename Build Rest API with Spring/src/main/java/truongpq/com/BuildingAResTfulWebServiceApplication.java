f2db6f30dc5539651d403f0e82de1115ed38bbcd
Pham Quang Truong <truongphamit@gmail.com>
2018-09-26 14:04:37 +0700

    init

---
+++ Build Rest API with Spring/src/main/java/truongpq/com/BuildingAResTfulWebServiceApplication.java
@@ -0,0 +1,12 @@
+package truongpq.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingAResTfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingAResTfulWebServiceApplication.class, args);
+    }
+}
