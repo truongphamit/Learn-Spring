0c133dcd9d6af638420828aa2ea8ac44d689b864
Pham Quang Truong <truong.pham@tripi.vn>
2018-09-27 11:14:06 +0700

    Accessing Relational Data using JDBC with Spring

---
+++ Accessing Relational Data using JDBC with Spring/src/main/java/com/truongpq/accessingrelationaldatausingjdbcwithspring/AccessingRelationalDataUsingJdbcWithSpringApplication.java
@@ -0,0 +1,50 @@
+package com.truongpq.accessingrelationaldatausingjdbcwithspring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class AccessingRelationalDataUsingJdbcWithSpringApplication implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccessingRelationalDataUsingJdbcWithSpringApplication.class);
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingRelationalDataUsingJdbcWithSpringApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE customers IF EXISTS ");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        splitUpNames.forEach(name -> LOGGER.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        LOGGER.info("Querying for customer records where first_name = 'Josh':");
+        jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[]{"Josh"},
+                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+        ).forEach(customer -> LOGGER.info(customer.toString()));
+    }
+}
