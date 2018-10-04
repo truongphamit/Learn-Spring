c8742b1df1933c5d05d934d184d2a13a5a28d079
Pham Quang Truong <truong.pham@tripi.vn>
2018-10-04 16:00:25 +0700

    Uploading files

---
+++ Uploading Files/src/main/java/com/truongpq/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,27 @@
+package com.truongpq.uploadingfiles;
+
+import com.truongpq.uploadingfiles.storage.StorageProperties;
+import com.truongpq.uploadingfiles.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingFilesApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
